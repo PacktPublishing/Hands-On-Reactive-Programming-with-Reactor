c5fe0da7e32ff920c386ef2d6e3dc4e5a16601be
Rahul Sharma <rahul0208@gmail.com>
2018-07-03 10:17:52 +0530

    Create ReactorMain.java

---
+++ Chapter05/reactorService/src/main/java/com/sample/ReactorMain.java
@@ -0,0 +1,33 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.ipc.netty.http.server.HttpServer;
+import reactor.util.function.Tuples;
+
+import java.time.Duration;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+
+
+@SpringBootApplication
+@Configuration
+@ComponentScan("com.sample.web")
+@EnableWebFlux
+public class ReactorMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorMain.class, args);
+    }
+}

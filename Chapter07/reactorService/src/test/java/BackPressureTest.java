f14c1b0515d96d0d6a07740991883b78150bcc9c
Rahul Sharma <rsharma@apache.org>
2018-07-30 22:52:32 +0530

    updating test

--- Chapter07/reactorService/src/test/java/BackPressureTest.java
+++ Chapter07/reactorService/src/test/java/BackPressureTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.util.function.Tuples;
@@ -17,21 +18,6 @@ import static org.junit.Assert.assertTrue;
 public class BackPressureTest {
 
     @Test
-    public  void testBackPressoreOps(){
-        Flux<Long> fibonacciGenerator = Flux.generate(() -> Tuples.<Long,
-                Long>of(0L, 1L), (state, sink) -> {
-            if (state.getT1() < 0)
-                sink.complete();
-            else
-                sink.next(state.getT1());
-            return Tuples.of(state.getT2(), state.getT1() + state.getT2());
-        });
-        fibonacciGenerator.sample(Duration.ofNanos(100))
-                .subscribe(x -> System.out.println(x));
-    }
-
-
-    @Test
     public  void testBackPressure() throws  Exception{
         Flux<Integer> numberGenerator = Flux.create(x -> {
             System.out.println("Requested Events :"+x.requestedFromDownstream());
@@ -45,6 +31,46 @@ public class BackPressureTest {
 
         CountDownLatch latch = new CountDownLatch(1);
         numberGenerator.subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(1);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println(value);
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                throwable.printStackTrace();
+                latch.countDown();
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                latch.countDown();
+            }
+        });
+        assertTrue(latch.await(1L, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public  void testBackPressureOps() throws  Exception{
+        Flux<Integer> numberGenerator = Flux.create(x -> {
+            System.out.println("Requested Events :"+x.requestedFromDownstream());
+            int number = 1;
+            while(number < 100) {
+                x.next(number);
+                number++;
+            }
+            x.complete();
+        });
+
+        CountDownLatch latch = new CountDownLatch(1);
+        numberGenerator
+                .onBackpressureBuffer(2,x -> System.out.println("Dropped :"+x),BufferOverflowStrategy.DROP_LATEST)
+                .subscribe(new BaseSubscriber<Integer>() {
             @Override
             protected void hookOnSubscribe(Subscription subscription) {
                 request(1);
